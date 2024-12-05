b11d8f6592e38190bc071a3e35c9e448d6491976
BismaSahr <sahrkhan487@gmail.com>
2024-12-06 01:17:42 +0500

    Basic Changes

--- header.php
+++ header.php
@@ -30,21 +30,8 @@ $categories = $conn->query($category_query);
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    <link rel="stylesheet" href="headerfooter.css">
     <style>
-      .custom-primary {
-        color: #ff5733 !important;
-      }
-      .custom-bg {
-        background-color: #885b96 !important;
-      }
-      .logo {
-         width: 100%;
-         height: auto;
-         max-width: 90px; 
-         max-height: 60px; 
-         border-radius: 30%;
-      }
 
       button:focus, input:focus, .btn:focus, .form-control:focus{
        outline: none !important;  
